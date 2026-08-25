b72b7937fb956cc835597ec55526930cef4449cb
Mario <mariox0603@gmail.com>
2026-08-25 16:34:36 -0500

    chore(deps): add rive-react-native@^9 and register riv asset ext

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,8 @@ const config = getDefaultConfig(__dirname);
 // Enable symlinks for pnpm compatibility
 config.resolver.unstable_enableSymlinks = true;
 
+// Las animaciones Rive se cargan con require(): sin registrar la extensión,
+// Metro no las resuelve como assets y el bundle revienta en tiempo de build.
+config.resolver.assetExts.push('riv');
+
 module.exports = config;
